c04df3955d81d7fb14b50a7eff60b2924c17ce48
Diego Meneses Perez <perezdi060606@gmail.com>
2024-06-30 10:49:45 -0600

    Ultimo commit

--- public/build/assets/InputLabel-C_PogVTN.js
+++
@@ -1,1 +0,0 @@
-import{j as n}from"./app-Cd6Eq6nm.js";function a({value:t,className:o="",children:r,...e}){return n.jsx("label",{...e,className:"text-gray-700 font-bold block "+o,children:t||r})}export{a as I};
